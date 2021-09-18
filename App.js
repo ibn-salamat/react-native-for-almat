1df40715df8dfce5b007decb08a67ab982bc34db
nurlykhan <nurlykhan.salamatuly@samgau.com>
2021-09-18 23:43:02 +0600

    feat: first steps

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: "red", fontSize: 40}}>Альхамдулиллах! Я айос разработчик</Text>
-      <Text style={{color: "green", fontSize: 30}}>Алмат тормоз</Text>
+      <Text style={{ color: "red", fontSize: 30 }}>Альхамдулиллах!</Text>
+      <Text style={{ color: "green", fontSize: 20 }}>Я айос разработчик</Text>
 
+      <Button
+        title="нажми на меня"
+        onPress={() => {
+          Alert.alert("спулае");
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
